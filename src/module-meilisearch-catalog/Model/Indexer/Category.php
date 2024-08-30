9024ce66d52ec48ad73f53c497f09b6596b22de2
Jimmy Carricart <jimmy@walkwizus.fr>
2024-08-30 07:24:47 +0200

    Revert "Update Meilisearch PHP SDK"
    This reverts commit 84ded4921502f89360dea13bfbfc8588136ea236.

--- src/module-meilisearch-catalog/Model/Indexer/Category.php
+++ src/module-meilisearch-catalog/Model/Indexer/Category.php
@@ -38,7 +38,6 @@ class Category implements \Magento\Framework\Indexer\ActionInterface, \Magento\F
     protected FullAction $fullAction;
 
     /**
-     * @param MeilisearchAdapter $meilisearchAdapter
      * @param StoreManagerInterface $storeManager
      * @param DimensionFactory $dimensionFactory
      * @param IndexerInterface $indexerHandler
